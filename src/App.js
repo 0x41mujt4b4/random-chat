ef5d32810d90da4cc9f0bbfd37ae9988b1a4e0eb
Almujtaba Abdalla <almujtaba.abdu@gmail.com>
2024-08-22 13:04:04 +0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import ChatPage from './components/Chat-page';
 import socketIO from 'socket.io-client';
+import Navbar from './components/Navbar';
+
 const socket = socketIO.connect('http://192.168.1.4:4000', {transports: ['websocket']});
+
 function App() {
   return (
       <BrowserRouter>
-        <div>
-           <Routes>
-              <Route path="/" element={<Home socket={socket} />}></Route>
-              <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
-            </Routes>
-        </div>
+        <Navbar socket={socket}/>
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />}></Route>
+          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+        </Routes>
       </BrowserRouter>
   );
 }
